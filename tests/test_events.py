d2004aefa0f14b02f036ce8fa90f3ecec4b43ee8
mjoss17 <matthewjoss17@gmail.com>
2022-10-25 14:45:08 -0500

    Fix linting errors

--- tests/test_events.py
+++ tests/test_events.py
@@ -8,8 +8,8 @@ from typing import Dict, Callable
 def test_post_events(
     client_factory: Callable[[Dict], TestClient], admin_header_factory: Callable[[str], Dict]
 ):
-    auth_keys = [{ "kid": "kid1", "secret":"secret1" },
-                 { "kid": "kid2", "secret":"secret2" }]
+    auth_keys = [{"kid":"kid1", "secret":"secret1"},
+                 {"kid":"kid2", "secret":"secret2"}]
     client = client_factory(auth_keys)
     auth_header = admin_header_factory(str(uuid.uuid4()), "kid1", "secret1")
     response = client.post("/v1/events", headers=auth_header)
@@ -20,10 +20,10 @@ def test_post_events(
 def test_post_two_different_kids(
     client_factory: Callable[[Dict], TestClient], admin_header_factory: Callable[[str], Dict]
 ):
-    auth_keys = [{ "kid": "kid1", "secret":"secret1" },
-                 { "kid": "kid2", "secret":"secret2" }]
+    auth_keys = [{"kid": "kid1", "secret": "secret1"},
+                 {"kid": "kid2", "secret": "secret2"}]
     client = client_factory(auth_keys)
-    
+
     auth_header = admin_header_factory(str(uuid.uuid4()), "kid1", "secret1" )
     response = client.post("/v1/events", headers=auth_header)
     assert response.status_code == 201
@@ -37,10 +37,12 @@ def test_post_two_different_kids(
 def test_expired_jwt(
     client_factory: Callable[[Dict], TestClient], admin_header_factory: Callable[[str], Dict]
 ):
-    auth_keys = [{ "kid": "kid1", "secret":"secret1" },
-                 { "kid": "kid2", "secret":"secret2" }]
+    auth_keys = [{"kid":"kid1", "secret":"secret1"},
+                 {"kid":"kid2", "secret":"secret2"}]
     client = client_factory(auth_keys)
 
-    auth_header = admin_header_factory(str(uuid.uuid4()), "key1", "secret1", expired=True)
+    auth_header = admin_header_factory(
+        str(uuid.uuid4()), "key1", "secret1", expired=True
+    )
     response = client.post("/v1/events", headers=auth_header)
     assert response.status_code == 403
