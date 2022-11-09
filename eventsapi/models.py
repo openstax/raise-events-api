2c6a8bcb3885112b2d238a97c13bafd1f73e9b5c
mjoss17 <matthewjoss17@gmail.com>
2022-11-09 16:20:31 -0600

    Create base models for content api

--- eventsapi/models.py
+++ eventsapi/models.py
@@ -1,5 +1,35 @@
-from pydantic import BaseModel
+from typing import Annotated, Literal, Union
+from pydantic import Field, BaseModel
+from uuid import UUID
 
 
 class DetailMessage(BaseModel):
     detail: str
+
+
+class BaseEvent(BaseModel):
+    course_id: int
+    impression_id: UUID
+    source_uri: str
+    timestamp: int
+
+
+class ContentLoadSuccessV1Event(BaseEvent):
+    eventname: Literal['content_load_success_v1']
+    content_id: UUID
+    variant: str
+
+
+class ContentLoadFailureV1Event(BaseEvent):
+    eventname: Literal['content_load_failure_v1']
+    content_id: UUID
+    details: str
+
+
+Event = Annotated[
+    Union[
+        ContentLoadSuccessV1Event,
+        ContentLoadFailureV1Event
+    ],
+    Field(discriminator='eventname')
+]
--- tests/test_events.py
+++ tests/test_events.py
@@ -10,8 +10,18 @@ def test_post_events(
     auth_keys = [{"kid": "kid1", "secret": "secret1"},
                  {"kid": "kid2", "secret": "secret2"}]
     client = client_factory(auth_keys)
+    body = [{
+        "course_id": 0,
+        "impression_id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+        "source_uri": "string",
+        "timestamp": 0,
+        "eventname": "content_load_success_v1",
+        "content_id": "3fa85f64-5717-4562-b3fc-2c963f66afa7",
+        "variant": "string"
+    }]
+
     auth_header = admin_header_factory(str(uuid.uuid4()), "kid1", "secret1")
-    response = client.post("/v1/events", headers=auth_header)
+    response = client.post("/v1/events", json=body, headers=auth_header)
     assert response.status_code == 201
     assert "detail" in response.json()
 
@@ -23,14 +33,23 @@ def test_post_two_different_kids(
     auth_keys = [{"kid": "kid1", "secret": "secret1"},
                  {"kid": "kid2", "secret": "secret2"}]
     client = client_factory(auth_keys)
+    body = [{
+         "course_id": 0,
+        "impression_id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+        "source_uri": "string",
+        "timestamp": 0,
+        "eventname": "content_load_success_v1",
+        "content_id": "3fa85f64-5717-4562-b3fc-2c963f66afa7",
+        "variant": "string"
+    }]
 
     auth_header = admin_header_factory(str(uuid.uuid4()), "kid1", "secret1")
-    response = client.post("/v1/events", headers=auth_header)
+    response = client.post("/v1/events", json=body, headers=auth_header)
     assert response.status_code == 201
     assert "detail" in response.json()
 
     auth_header = admin_header_factory(str(uuid.uuid4()), "kid2", "secret2")
-    response = client.post("/v1/events", headers=auth_header)
+    response = client.post("/v1/events", json=body, headers=auth_header)
     assert response.status_code == 201
     assert "detail" in response.json()
 
@@ -78,3 +97,99 @@ def test_invalid_token(
     response = client.post("/v1/events", headers=auth_header)
     assert response.status_code == 403
     assert response.json()['detail'] == 'Error decoding token headers.'
+
+
+def test_content_load_success_event(
+    client_factory: Callable[[Dict], TestClient],
+    admin_header_factory: Callable[[str], Dict]
+):
+    auth_keys = [{"kid": "kid1", "secret": "secret1"}]
+    client = client_factory(auth_keys)
+
+    auth_header = admin_header_factory(str(uuid.uuid4()), "kid1", "secret1")
+    body = [{
+         "course_id": 0,
+        "impression_id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+        "source_uri": "string",
+        "timestamp": 0,
+        "eventname": "content_load_success_v1",
+        "content_id": "3fa85f64-5717-4562-b3fc-2c963f66afa7",
+        "variant": "string"
+    }]
+    response = client.post("/v1/events", json=body, headers=auth_header)
+    assert response.status_code == 201
+    assert "detail" in response.json()
+
+
+def test_content_load_failure_event(
+    client_factory: Callable[[Dict], TestClient],
+    admin_header_factory: Callable[[str], Dict]
+):
+    auth_keys = [{"kid": "kid1", "secret": "secret1"}]
+    client = client_factory(auth_keys)
+
+    auth_header = admin_header_factory(str(uuid.uuid4()), "kid1", "secret1")
+    body = [{
+         "course_id": 0,
+        "impression_id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+        "source_uri": "string",
+        "timestamp": 0,
+        "eventname": "content_load_failure_v1",
+        "content_id": "3fa85f64-5717-4562-b3fc-2c963f66afa7",
+        "details": "string"
+    }]
+    response = client.post("/v1/events", json=body, headers=auth_header)
+    assert response.status_code == 201
+    assert "detail" in response.json()
+
+
+def test_multiple_events(
+    client_factory: Callable[[Dict], TestClient],
+    admin_header_factory: Callable[[str], Dict]
+):
+    auth_keys = [{"kid": "kid1", "secret": "secret1"}]
+    client = client_factory(auth_keys)
+
+    auth_header = admin_header_factory(str(uuid.uuid4()), "kid1", "secret1")
+    body = [{
+         "course_id": 0,
+        "impression_id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+        "source_uri": "string",
+        "timestamp": 0,
+        "eventname": "content_load_success_v1",
+        "content_id": "3fa85f64-5717-4562-b3fc-2c963f66afa7",
+        "variant": "string"
+    },
+    {
+         "course_id": 0,
+        "impression_id": "3fa85f64-5717-4562-b3fc-2c963f66af23",
+        "source_uri": "string",
+        "timestamp": 0,
+        "eventname": "content_load_failure_v1",
+        "content_id": "3fa85f64-5717-4562-b3fc-2c963f66af45",
+        "details": "string"
+    }]
+    response = client.post("/v1/events", json=body, headers=auth_header)
+    assert response.status_code == 201
+    assert "detail" in response.json()
+
+
+def test_invalid_event_body(
+    client_factory: Callable[[Dict], TestClient],
+    admin_header_factory: Callable[[str], Dict]
+):
+    auth_keys = [{"kid": "kid1", "secret": "secret1"}]
+    client = client_factory(auth_keys)
+
+    auth_header = admin_header_factory(str(uuid.uuid4()), "kid1", "secret1")
+    body = [{
+        "course_id": "abc",
+        "impression_id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+        "content_id": "string",
+        "eventname": "content_loaded_v1",
+        "source_uri": "string",
+        "timestamp": 0
+    }]
+    response = client.post("/v1/events", json=body, headers=auth_header)
+    assert response.status_code == 422
+    assert "detail" in response.json()
