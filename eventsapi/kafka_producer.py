6c2c6102eb676741b61200f1be18f72646face82
Ripal Nathuji <rnathuji@neoteklabs.com>
2022-12-06 16:47:18 -0600

    Modify kafka producer to use sasl auth

--- eventsapi/kafka_producer.py
+++ eventsapi/kafka_producer.py
@@ -1,12 +1,13 @@
 import boto3
 from aiokafka import AIOKafkaProducer
+from aiokafka.helpers import create_ssl_context
 from aws_schema_registry import SchemaRegistryClient
 from kafka.serializer import Serializer
 from aws_schema_registry import DataAndSchema
 from aws_schema_registry.adapter.kafka import KafkaSerializer
 from eventsapi.settings import \
-    GLUE_REGISTRY_NAME, AWS_REGION, \
-    KAFKA_BOOTSTRAP_BROKERS
+    GLUE_REGISTRY_NAME, GLUE_AWS_REGION, KAFKA_BOOTSTRAP_BROKERS, \
+    KAFKA_SASL_USERNAME, KAFKA_SASL_PASSWORD
 
 
 class KafkaAvroSerializer(Serializer):
@@ -28,9 +29,13 @@ def get_kafka_producer():
         serializer = get_kafka_glue_serializer()
         producer = AIOKafkaProducer(
             bootstrap_servers=KAFKA_BOOTSTRAP_BROKERS,
-            security_protocol="SSL",
             value_serializer=build_callable_serializer(serializer),
-            acks='all'
+            acks='all',
+            security_protocol="SASL_SSL",
+            ssl_context=create_ssl_context(),
+            sasl_mechanism="SCRAM-SHA-512",
+            sasl_plain_username=KAFKA_SASL_USERNAME,
+            sasl_plain_password=KAFKA_SASL_PASSWORD
         )
         return producer
     else:
@@ -43,7 +48,7 @@ def get_kafka_producer():
 
 
 def get_kafka_glue_serializer():
-    glue_client = boto3.client('glue', region_name=AWS_REGION)
+    glue_client = boto3.client('glue', region_name=GLUE_AWS_REGION)
     client = SchemaRegistryClient(
         glue_client,
         registry_name=GLUE_REGISTRY_NAME
